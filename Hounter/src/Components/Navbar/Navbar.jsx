e4efefb789e06f2a3d67317e9db796696b49b787
dicksonmutwiriguantai <dickson.mutwiri@student.moringaschool.com>
2024-04-24 19:33:57 +0300

    fixed overflow on tablets for the nav dropdown

--- Hounter/src/Components/Navbar/Navbar.jsx
+++ Hounter/src/Components/Navbar/Navbar.jsx
@@ -95,7 +95,7 @@ export default function Navbar() {
               <button
                 id="dropdownNavbarLink"
                 onClick={toggleDropdown}
-                className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+                className="flex items-center justify-between w-full py-2 px-2 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
               >
                 Account
                 <svg
@@ -117,7 +117,7 @@ export default function Navbar() {
               <div
                 className={`${
                   isDropdownOpen ? 'absolute' : 'hidden'
-                } z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600`}
+                } right-4 z-10 font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600`}
               >
                 <ul
                   className="py-2 text-sm text-gray-700 dark:text-gray-400"
