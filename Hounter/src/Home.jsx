b233e9d48e64635a8fba961f9b23ba322eb09a40
dicksonmutwiriguantai <dickson.mutwiri@student.moringaschool.com>
2024-04-24 21:42:36 +0300

    search fields layout

--- Hounter/src/Home.jsx
+++ Hounter/src/Home.jsx
@@ -1,13 +1,14 @@
 // Home.jsx
 import Navbar from "./Components/Navbar/Navbar";
 import Footerall from "./Components/Footer/Footer";
+import SearchBar from "./Components/SearchBar/SearchBar";
 export default function Home() {
   
 
   return (
     <>
     <Navbar/>
-
+    <SearchBar/>
     <Footerall/>
       
     </>
