37b5f94b7087e7afc39d98e574f602a8d2f649f3
dicksonmutwiriguantai <dickson.mutwiri@student.moringaschool.com>
2024-04-26 15:44:47 +0300

    search input fileds fix types

--- Hounter/src/Components/SearchBar/SearchBar.jsx
+++ Hounter/src/Components/SearchBar/SearchBar.jsx
@@ -7,13 +7,13 @@ export default function SearchBar() {
         <p className="md:text-4xl text-xl md:m-8">Find any House in 254</p>
         <div className="flex flex-col md:flex-row md:justify-between md:items-center  md:m-8">
           <div className="md:mr-2">
-            <input type="email" placeholder="I am looking for..." className="mt-2 w-3/4 md:w-full placeholder-gray-400/70 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300" />
+            <input type="search" placeholder="I am looking for..." className="mt-2 w-3/4 md:w-full placeholder-gray-400/70 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300" />
           </div>
           <div className="md:mr-2 mt-3 md:mt-0">
-            <input type="password" placeholder="Area..." className="mt-2 w-3/4 md:w-full placeholder-gray-400/70 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300" />
+            <input type="search" placeholder="Area..." className="mt-2 w-3/4 md:w-full placeholder-gray-400/70 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300" />
           </div>
           <div>
-            <input type="password" placeholder="Amount..." className="mt-2 w-3/4 md:w-full placeholder-gray-400/70 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300" />
+            <input type="search" placeholder="Amount..." className="mt-2 w-3/4 md:w-full placeholder-gray-400/70 rounded-lg border border-gray-200 bg-white px-5 py-2.5 text-gray-700 focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40 dark:border-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-blue-300" />
           </div>
         </div>
       </div>
