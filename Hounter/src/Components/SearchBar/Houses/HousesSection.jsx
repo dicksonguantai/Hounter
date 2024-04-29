a5132865ea0702f430355cca3c331ff7b75c99f5
dicksonmutwiriguantai <dickson.mutwiri@student.moringaschool.com>
2024-04-29 17:25:32 +0300

    better cards

--- Hounter/src/Components/SearchBar/Houses/HousesSection.jsx
+++ Hounter/src/Components/SearchBar/Houses/HousesSection.jsx
@@ -3,13 +3,13 @@ import HouseCard from "./HouseCard";
 export default function HousesSection(){
 
     return (
-        <>
+        <div className="flex flex-wrap mx-auto">
         <HouseCard/>
         <HouseCard/>
         <HouseCard/>
         <HouseCard/>
         <HouseCard/>
 
-        </>
+        </div>
     );
 }
--- Hounter/src/Home.jsx
+++ Hounter/src/Home.jsx
@@ -2,21 +2,18 @@
 import Navbar from "./Components/Navbar/Navbar";
 import Footerall from "./Components/Footer/Footer";
 import SearchBar from "./Components/SearchBar/SearchBar";
-import HouseCard from "./Components/SearchBar/Houses/HouseCard";
 import HousesSection from "./Components/SearchBar/Houses/HousesSection";
 export default function Home() {
   
 
   return (
-    <section>
-    <div className="flex flex-wrap">
+    <div>
     <Navbar/>
     <SearchBar/>
     <HousesSection/>
     <Footerall/>
 
       
-    </div></section>
-    
+    </div>
     );
 }
