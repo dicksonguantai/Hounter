80043c2cb59607d838ac4e344c888c43c56b30ce
dicksonmutwiriguantai <dickson.mutwiri@student.moringaschool.com>
2024-05-22 21:28:41 +0300

    consuming individual house api

--- Hounter/src/Components/Houses/HouseCard.jsx
+++ Hounter/src/Components/Houses/HouseCard.jsx
@@ -16,12 +16,12 @@ import {
 import BookingVisit from "../VisitBooking/BookingVisit";
 
 
-export default function HouseCard({ onBookNow }) {
-  const [images, setImages] = useState([
-    "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-    "https://images.unsplash.com/photo-1587613892572-86e79e7d2225?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80",
-    "https://images.unsplash.com/photo-1563013544-824ae0efbdc8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-  ]);
+export default function HouseCard({house}) {
+  const { id, price, location, description, name, gate_id, rooms_available, housetype_id, gate, images } = house;
+
+  console.log(images);
+
+  
 
   const [currentSlide, setCurrentSlide] = useState(0);
 
@@ -47,18 +47,7 @@ export default function HouseCard({ onBookNow }) {
     }
   };
 
-  const handleBookNow = () => {
-    // Assuming house details are available here
-    const houseDetails = {
-      price: "Kshs 7,000",
-      type: "Bedsitter",
-      location: "Juja",
-      description: "House description...",
-      image: images[currentSlide],
-    };
-    // Call the function to open the modal and pass house details
-    onBookNow(houseDetails);
-  };
+  
 
   return (
     <div className="overflow-hidden m-4 bg-white rounded-lg shadow-lg dark:bg-gray-800">
@@ -73,7 +62,7 @@ export default function HouseCard({ onBookNow }) {
               />
               <img
                 className="object-cover object-center w-full h-56"
-                src={image}
+                src={image.url}
                 alt={`House Image ${index}`}
               />
               <FontAwesomeIcon
@@ -88,20 +77,20 @@ export default function HouseCard({ onBookNow }) {
 
       <div className="flex items-center justify-center px-6 py-3 bg-gray-900">
         <h1 className="text-lg font-semibold text-white">
-          <FontAwesomeIcon icon={faHouseUser} /> Kshs 7,000
+          <FontAwesomeIcon icon={faHouseUser} /> Kshs {price}
         </h1>
       </div>
 
       <div className="px-6 py-4">
         <h1 className="text-xl font-semibold text-gray-800 dark:text-white">
-          Bedsitter
+        {housetype_id}
         </h1>
         <h1 className="text-xl font-semibold text-gray-800 dark:text-white">
-          <FontAwesomeIcon icon={faDoorOpen} /> Gate A
+          <FontAwesomeIcon icon={faDoorOpen} /> {gate.name}
         </h1>
 
         <p className="py-2 text-gray-700 dark:text-gray-400">
-          House description...
+          {description}
         </p>
 
         <div className="flex items-center mt-4 text-gray-700 dark:text-gray-200">
@@ -123,20 +112,10 @@ export default function HouseCard({ onBookNow }) {
               d="M5.79417 16.5183C2.19424 13.0909 2.05438 7.3941 5.48178 3.79418C8.90918 0.194258 14.6059 0.0543983 18.2059 3.48179C21.8058 6.90919 21.9457 12.606 18.5183 16.2059L12.3124 22.7241L5.79417 16.5183ZM17.0698 14.8268L12.243 19.8965L7.17324 15.0698C4.3733 12.404 4.26452 7.9732 6.93028 5.17326C9.59603 2.37332 14.0268 2.26454 16.8268 4.93029C19.6267 7.59604 19.7355 12.0269 17.0698 14.8268Z"
             />
           </svg>
-          <h1 className="px-2 text-sm">Juja</h1>
+          <h1 className="px-2 text-sm">{location}</h1>
         </div>
 
         <div className="flex items-center justify-center mt-4 text-gray-700 dark:text-gray-200">
-          {/* <button
-            onClick={handleBookNow}
-            className="flex items-center bg-blue-500 text-white font-semibold px-4 py-2 rounded-md hover:bg-blue-600"
-          >
-            <h1 className="px-2 text-sm">Book Now</h1>
-            <FontAwesomeIcon
-              icon={faCalendarDays}
-              className="w-6 h-6 fill-current"
-            />
-          </button> */}
           <BookingVisit />
         </div>
       </div>
--- Hounter/src/Components/Houses/HousesSection.jsx
+++ Hounter/src/Components/Houses/HousesSection.jsx
@@ -1,13 +1,22 @@
+import { useState, useEffect } from "react";
 import HouseCard from "./HouseCard";
 
-export default function HousesSection() {
+const HousesSection = () => {
+  const [houses, setHouses] = useState([]);
+
+  useEffect(() => {
+    fetch("https://4g90ay7oei.execute-api.us-east-1.amazonaws.com/api/houses")
+      .then((response) => response.json())
+      .then((data) => setHouses(data))
+      .catch((error) => console.error("Error fetching houses:", error));
+  }, []);
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      <HouseCard />
-      <HouseCard />
-      <HouseCard />
-      <HouseCard />
-      <HouseCard />
+      {houses.map((house) => (
+        <HouseCard key={house.id} house={house} />
+      ))}
     </div>
   );
-}
+};
+
+export default HousesSection;
